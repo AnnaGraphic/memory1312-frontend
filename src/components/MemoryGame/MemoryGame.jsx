82078cb50c4f71585f5029ca7a53fcb81b4bca91
panda <p4nd4@posteo.de>
2024-04-10 12:11:49 +0200

    correct indentation

--- src/components/MemoryGame/MemoryGame.jsx
+++ src/components/MemoryGame/MemoryGame.jsx
@@ -6,80 +6,84 @@ import { useGameContext } from '../../contexts/gameContext';
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 function MemoryGame() {
-    const { state, dispatch } = useGameContext();
+  const { state, dispatch } = useGameContext();
 
-    useEffect(() => {
-      const fetchInitialCardsData = async () => {
-        try {
-          const response = await fetch(`${baseUrl}/cards`);
-          if (!response.ok) {
-            throw new Error(`Failed to fetch cards: ${response.status} - ${response.statusText}`);
-          }
-          const cardsData = await response.json();
-          dispatch({ type: 'SET_CARDS_DATA', payload: shuffleArray(cardsData) });
-        } catch (error) {
-          console.error(error);
+  useEffect(() => {
+    const fetchInitialCardsData = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/cards`);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch cards: ${response.status} - ${response.statusText}`);
         }
-      };
-
-      fetchInitialCardsData();
-    }, []);
-
-    useEffect(() => {
-      checkForMatch();
-    }, [state.secondCard]);
-
-    function shuffleArray(array) {
-      let idCounter = 0;
-      // double cards to get pairs        
-      const pairsArray = array.flatMap(card => [{ ...card, id: idCounter++ }, { ...card, id: idCounter++ }]);
-      for (let i = pairsArray.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [pairsArray[i], pairsArray[j]] = [pairsArray[j], pairsArray[i]];
+        const cardsData = await response.json();
+        dispatch({ type: 'SET_CARDS_DATA', payload: shuffleArray(cardsData) });
+      } catch (error) {
+        console.error(error);
       }
-      return pairsArray;
+    };
+
+    fetchInitialCardsData();
+  }, []);
+
+  useEffect(() => {
+    checkForMatch();
+  }, [state.secondCard]);
+
+  function shuffleArray(array) {
+    let idCounter = 0;
+    // double cards to get pairs
+    const pairsArray = array.flatMap(card => [{ ...card, id: idCounter++ }, { ...card, id: idCounter++ }]);
+    for (let i = pairsArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [pairsArray[i], pairsArray[j]] = [pairsArray[j], pairsArray[i]];
     }
+    return pairsArray;
+  }
 
-    const checkForMatch = () => {
-      console.log('checkForMatch');
-      const { firstCard, secondCard } = state;
-      if (firstCard && secondCard) {
-        console.log('firstCard: ', firstCard.name, 'secondCard: ', secondCard.name);
-        if (firstCard.name === secondCard.name) {
-          dispatch({ type: 'LOCK_BOARD', payload: true });
-          setTimeout(() => {
-            console.log('match');
-            dispatch({ type: 'LOCK_BOARD', payload: false });
-            dispatch({ type: 'FLIP_CARD', payload: firstCard });
-            dispatch({ type: 'FLIP_CARD', payload: secondCard });
-            lockCards();
-          }, 1600);
-        } else {
-          dispatch({ type: 'LOCK_BOARD', payload: true });
-          setTimeout(() => {
-            dispatch({ type: 'FlIP_CARD', payload: firstCard });
-            dispatch({ type: 'FlIP_CARD', payload: secondCard });
-            dispatch({ type: 'SET_FIRST_CARD', payload: null });
-            dispatch({ type: 'SET_SECOND_CARD', payload: null });
-            dispatch({ type: 'UNFLIP_CARDS', payload: [firstCard.id, secondCard.id] });
-            dispatch({ type: 'LOCK_BOARD', payload: false });
-          }, 1600);
-        }
+  const checkForMatch = () => {
+    console.log('checkForMatch');
+    const { firstCard, secondCard } = state;
+    if (firstCard && secondCard) {
+      console.log('firstCard: ', firstCard.name, 'secondCard: ', secondCard.name);
+      if (firstCard.name === secondCard.name) {
+        dispatch({ type: 'LOCK_BOARD', payload: true });
+        setTimeout(() => {
+          console.log('match');
+          dispatch({ type: 'LOCK_BOARD', payload: false });
+          dispatch({ type: 'FLIP_CARD', payload: firstCard });
+          dispatch({ type: 'FLIP_CARD', payload: secondCard });
+          lockCards();
+        }, 1600);
+      } else {
+        dispatch({ type: 'LOCK_BOARD', payload: true });
+        setTimeout(() => {
+          dispatch({ type: 'FlIP_CARD', payload: firstCard });
+          dispatch({ type: 'FlIP_CARD', payload: secondCard });
+          dispatch({ type: 'SET_FIRST_CARD', payload: null });
+          dispatch({ type: 'SET_SECOND_CARD', payload: null });
+          dispatch({ type: 'UNFLIP_CARDS', payload: [firstCard.id, secondCard.id] });
+          dispatch({ type: 'LOCK_BOARD', payload: false });
+        }, 1600);
       }
-    };
+    }
+  };
 
-    const handleCardClick = (card) => {
-      console.log('card clicked: ', card.name);
-      if (state.lockBoard) return;
-      if (!state.firstCard) {
-        dispatch({ type: 'SET_FIRST_CARD', payload: card });
-        console.log('firstCard: ', state.firstCard);
-      } else if (!state.secondCard) {
-        dispatch({ type: 'SET_SECOND_CARD', payload: card });
-        //checkForMatch();
-      }
-      dispatch({ type: 'FLIP_CARD', payload: card.id });
-    };
+  const handleCardClick = (card) => {
+    console.log('card clicked: ', card.name);
+    if (state.lockBoard) return;
+    if (!state.firstCard) {
+      dispatch({ type: 'SET_FIRST_CARD', payload: card });
+      console.log('firstCard: ', state.firstCard);
+    } else if (!state.secondCard) {
+      dispatch({ type: 'SET_SECOND_CARD', payload: card });
+      dispatch({ type: 'SWITCH_PLAYER' });
+
+      // moved to useEffect to update state:
+      //checkForMatch();
+    }
+    dispatch({ type: 'FLIP_CARD', payload: card.id });
+  };
+
 
     return (
         <div className="container">
